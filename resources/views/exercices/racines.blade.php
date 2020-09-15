a77caea37b16fdbf41b95d7eb105c54e8547aa3b
basilgass <basil.gass@bluewin.ch>
2020-09-15 08:06:09 +0200

    Initial release for testing

--- resources/views/exercices/racines.blade.php
+++ resources/views/exercices/racines.blade.php
@@ -1,6 +1,6 @@
 <div>
     <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-    <h1>Racines</h1>
+    <h1>Racines et d'autres contenu</h1>
 
     <div>
         \(\sqrt{12}=2\sqrt{3}\)
